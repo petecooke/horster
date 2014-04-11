0a58a2564da576e73efc0023ecf43b66889adc2f
Pete Cooke <pete.cooke.81@gmail.com>
2014-04-11 01:33:15 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Horster::Application.config.secret_key_base = '685be63ce686a563dc4d4cb9473b2ee4bbd829d1a8a89d8d352ed91f9c4c135e782ac1753aaec35cccbde65a5519d865fa9933b6f0346218e3759a22ecc6abc4'
