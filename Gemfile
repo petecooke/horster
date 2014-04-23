32915847ec8d309fe02ede54cc86e1fbac8bd2e0
Pete Cooke <pete.cooke.81@gmail.com>
2014-04-23 03:16:39 +0000

    Added form for new places

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 gem 'bootstrap-sass', '~> 3.1.1'
+gem 'simple_form'
 
 # Gemfile for Rails 3+, Sinatra, or Merb
 gem 'will_paginate', '~> 3.0.5'
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Horster::Application.routes.draw do
   # You can have the root of your site routed with "root"
     root 'places#index'
 
+    resources :places
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
