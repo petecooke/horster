aafb5a49ac6b01b7b86f3b389595c560c18dd9c0
Pete Cooke <pete.cooke.81@gmail.com>
2014-06-22 19:28:03 +0000

    turned off user check for destroy in order to clear out old objects

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-	before_action :authenticate_user!
+	# before_action :authenticate_user!
 	def create
 		@place = Place.find(params[:place_id])
 		@place.photos.create(photo_params.merge(:user => current_user))
